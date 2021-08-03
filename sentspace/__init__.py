75a07839d88e528de4c1b508d24bf55bafdec281
aalok-sathe <asathe@mit.edu>
2021-08-03 17:39:12 -0400

    add module root init file

---
+++ sentspace/__init__.py
@@ -0,0 +1,17 @@
+_license_string = '''
+    (C) 2020-21 EvLab, MIT BCS. All rights reserved.
+    
+    Please contact Greta Tuckute <gretatu@mit.edu> 
+    for license-related questions.
+'''
+
+def license():
+    print(_license_string)
+
+
+import sentspace.syntax as syntax
+import sentspace.lexical as lexical
+# import sentspace.semantic as semantic
+# import sentspace.embedding as embedding
+# ...
+import sentspace.utils
