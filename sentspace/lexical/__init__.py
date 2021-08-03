2da3043e414977f4cda00cb69f98673211880ec5
aalok-sathe <asathe@mit.edu>
2021-08-03 17:48:39 -0400

    create submodule for lexical features

--- sentspace/lexical/__init__.py
+++ sentspace/lexical/__init__.py
@@ -1,3 +1,50 @@
 import os
 from pathlib import Path
+from typing import List
 
+
+def get_lexical_features(sentence:List[str]):
+
+    return 
+    
+    merged_vals = utils.get_all_features_merged(
+        flat_cleaned_token_list, flat_lemmatized_token_list, databases)  # lexical features
+    # Clear variables so we have RAM
+    del databases
+
+    # Results
+    result = utils.compile_results(flat_token_list, flat_cleaned_token_list, flat_lemmatized_token_list,
+                                flat_pos_tags, flat_is_content_word, setlst,
+                                flat_sentence_num, flat_token_lens, merged_vals)
+
+    result = utils.transform_features(result, *features_transform)
+
+    features_used = ['Age of acquisition', 'Arousal', 'Concreteness',
+                    'Contextual diversity (log)', 'Degree centrality (log)',
+                    'Frequency of orthographic neighbors (log)', 'Lexical decision RT',
+                    'Lexical frequency (log)', 'Lexical surprisal', 'Number of morphemes', 'Number of morphemes poly',
+                    'Orthography-Semantics Consistency', 'Prevalence', 'Valence', 'Word length', 'Polysemy']
+
+    print('Computing sentence embeddings')
+    sent_embed = utils.get_sent_vectors(result, features_used, embed_method,
+                                        content_only=content_only,
+                                        pronoun_ratios=pronoun_ratios,
+                                        )
+    lex_per_word_with_uniform_column = utils.conform_word_lex_df_columns(
+        result)
+    lex_per_word_with_uniform_column.to_csv(word_lex_output_path, index=False)
+
+    print('Writing lex sentence embedding to csv at ' + embed_lex_output_path)
+    sent_embed.to_csv(embed_lex_output_path, index=False)
+
+    # Make the syntax excel sheet
+    print('Writing syntax sentence embedding to csv at ' + sent_output_path)
+    #pdb.set_trace()
+
+    # Read in benchmark data
+    df_benchmark = pd.read_csv(benchmark_file)
+
+    # Return percentile per sentence for each
+    percentile_df = utils.return_percentile_df(df_benchmark, sent_embed)
+    print('Writing percentiles')
+    percentile_df.to_csv(bench_perc_out_path, index=False)
--- sentspace/lexical/__main__.py
+++ sentspace/lexical/__main__.py
@@ -1,9 +1,36 @@
 import argparse
-import sentspace.lexical
+from sentspace.lexical import get_features
 
 
 def main(args):
-    pass
+    
+    print('Estimating lexical features')
+
+
 
 if __name__ == '__main__':
-    main()
+    # Parse input
+    parser = argparse.ArgumentParser('sentspace.lexical')
+
+    parser.add_argument('input', type=str, #argparse.FileType('r'), 
+                        # default=sys.stdin,
+                        help='File (currently NotImplemented) or single sentence to process')
+
+    # Add an option for a user to include their own stop words
+    parser.add_argument('-sw', '--stop_words', default=None,
+                        help='Path to delimited file of words to filter out from analysis. For example example/stopwords.txt')
+
+    # Add an option for a user to choose their benchmark
+    parser.add_argument('-b', '--benchmark', default='sentspace/benchmarks/lex/UD_corpora_lex_features_sents_all.csv',
+                        help='Path to csv file of benchmark corpora For example benchmarks/lex/UD_corpora_lex_features_sents_all.csv')
+
+    # Add an option for a user to choose to not do some analyses. Default is true
+    parser.add_argument('-dlt', '--dlt', type=strtobool,
+                        default=True, help='calculate dependency locality theory (DLT) metric? [True]')
+    parser.add_argument('-lc', '--left_corner', type=strtobool,
+                        default=True, help='calculate left corner metric? [True]')
+
+    args = parser.parse_args()
+    print(args)
+    
+    main(args)
