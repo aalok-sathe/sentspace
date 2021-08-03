f184562c4c3d2b2e75df9fb051a7917852f1afc0
aalok-sathe <asathe@mit.edu>
2021-08-03 17:40:46 -0400

    update many variable names and method calls according to new convention

--- sentspace/__main__.py
+++ sentspace/__main__.py
@@ -1,25 +1,32 @@
 # This file accepts as input a path to a file containing sentences and 
 # prints out a csv with sentences per row and sentence features per column.
 
-print('Loading modules...')
+# print('Loading modules... (chunk 1/4)', end='\r')
 
 import argparse
 import os
+import pathlib
 import sys
 from datetime import date
 from distutils.util import strtobool
 
+# print('Loading modules... (chunk 2/4)', end='\r')
 import nltk
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from nltk.corpus import wordnet
 
-import sentspace.utils as utils
-from sentspace.sanity_checks import sanity_check_databases
+# print('Loading modules... (chunk 3/4)', end='\r')
 import sentspace.syntax
+import sentspace.utils as utils
+from sentspace.utils import wordnet
+# from sentspace.utils.caching import cache_to_disk, cache_to_mem
+# from sentspace.utils.text import get_flat_pos_tags
+# from sentspace.utils.utils import wordnet
 
+# print('Loading modules... (chunk 4/4)', end='\r')
 import matplotlib.pyplot as plt; plt.rcdefaults()
+
 
 # download NLTK data if not already downloaded
 for nltk_resource in ['taggers/averaged_perceptron_tagger', 'corpora/wordnet']:
@@ -51,38 +58,27 @@ for nltk_resource in ['taggers/averaged_perceptron_tagger', 'corpora/wordnet']:
 # from s3 import load_feature
 
 
-def estimate_sentence_embeddings(path_2_sentence_file, path_2_stop_words, path_2_bechmark,
-								 calc_lex, calc_syntax, calc_glove,**kwargs):
-								 	
-	#pdb.set_trace()
-	stop_words = None
-	if path_2_stop_words:
-		stop_words = np.loadtxt(path_2_stop_words, delimiter='\t', unpack=False, dtype=str)
+def run_sentence_features_pipeline(input_file:pathlib.Path, stop_words_file:str=None, 
+                                   benchmark_file: str = None, out_dir: str = None,
+                                   lexical=False, syntax=False, embedding=False, semantic=False):
+	"""runs the full sentence features pipeline on the given input according to
+		requested submodules (lexical, syntax, ...).
+		returns output in the form of TODO
 
-	print('Loading feature databases...')
-	databases = utils.load_databases(
-		features=['NRC_Arousal', 'NRC_Valence', 'OSC', 'aoa', 'concreteness',
-				  'lexical_decision_RT', 'log_contextual_diversity', 
-				  'log_lexical_frequency', 'n_orthographic_neighbors', 'num_morpheme',
-				  'prevalence', 'surprisal-3', 'total_degree_centrality'])
-
-	print('Performing sanity checks...')
-	sanity_check_databases()(databases)
-
-	print('Parsing input sentences...')
-	plot_dist = False
-	if plot_dist == True:
-		features = []
-		values = []
-		for feat, d in databases.items():
-		    values.extend(d.values())
-		    features.extend([feat]*len(d))
-		df = pd.DataFrame({'feature': features, 'value': values})
-		print('Preparing plots')
-		g = sns.FacetGrid(df, col='feature', col_wrap=3, sharex=False, sharey=False)
-		g.map(sns.distplot, 'value')
-		plt.show()
+	Args:
+		input_file (str): path to input text file containing sentences
+							one per line [required]
+		stop_words_file (str): path to text file containing stopwords to filter
+								out, one per line [optional]
+		benchmark_file (str): path to a file containing a benchmark corpus to
+								compare the current input against; e.g. UD [optional]
 		
+		{lexical,syntax,embedding,semantic,...} (bool): compute submodule features? [False]
+	"""
+
+	databases = utils.io.load_databases(features='all')
+
+
 	# Sentence vectors settings
 	embed_method = 'all' # options: 'strict', 'all'
 	content_only = False
@@ -90,21 +86,21 @@ def estimate_sentence_embeddings(path_2_sentence_file, path_2_stop_words, path_2
 	# Define output paths
 	default = True
 	suffix = ''
-	out_file_name = os.path.basename(path_2_sentence_file).split('.')[0]
+	out_file_name = os.path.basename(input_file).split('.')[0]
 	if default:
 	    date_ = date.today().strftime('%m%d%Y')
 	    output_folder = f'output_folder/{date_}'
 	    sent_suffix = f"_{embed_method}"
 	    if content_only:
 	        sent_suffix = '_content' + sent_suffix
-	    if path_2_stop_words is not None:
+	    if stop_words_file is not None:
 	        sent_suffix = '_content'+'_minus_stop_words' + sent_suffix
 	    # Make out outlex path    
-	    word_lex_output_path, embed_lex_output_path, plot_path, na_words_path, bench_perc_out_path = utils.create_output_path(output_folder, out_file_name, 'lex', sent_suffix=sent_suffix)
+	    word_lex_output_path, embed_lex_output_path, plot_path, na_words_path, bench_perc_out_path = utils.io.create_output_path(output_folder, out_file_name, 'lex', sent_suffix=sent_suffix)
 	    # Make output syntax path
-	    _, sent_output_path = utils.create_output_path(output_folder, out_file_name, 'syntax', sent_suffix=sent_suffix)
-	    glove_words_output_path, glove_sents_output_path = utils.create_output_path(output_folder, out_file_name, 'glove', sent_suffix=sent_suffix)
-	    pmi_paths = utils.create_output_path(output_folder, out_file_name, 'PMI', sent_suffix=sent_suffix)
+	    _, sent_output_path = utils.io.create_output_path(output_folder, out_file_name, 'syntax', sent_suffix=sent_suffix)
+	    glove_words_output_path, glove_sents_output_path = utils.io.create_output_path(output_folder, out_file_name, 'glove', sent_suffix=sent_suffix)
+	    pmi_paths = utils.io.create_output_path(output_folder, out_file_name, 'PMI', sent_suffix=sent_suffix)
 	    #pdb.set_trace()
 	    lex_base = f'analysis_example/{date_}\\lex\\'
 	else: # custom path
@@ -113,107 +109,60 @@ def estimate_sentence_embeddings(path_2_sentence_file, path_2_stop_words, path_2
 	    na_words_path = ''
 	    embed_output_path = ''
 
-	# Prepare variables
-	sent_path = path_2_sentence_file # sentence file
-	sent_tokens, sent_rows, _ = utils.import_sentences(sent_path, sent_only = True,stop_words=stop_words)
-	setlst = None # list of set no.
-	
-	lemmatized_pos = [wordnet.ADJ, wordnet.VERB, wordnet.NOUN, wordnet.ADV] # define POS used for lemmatization
-	content_pos = [wordnet.ADJ, wordnet.VERB, wordnet.NOUN, wordnet.ADV] # define POS that count as content words
-	clean_word_method = 'punctuation' 
+	token_lists, sentences = utils.io.read_sentences(input_file, stop_words_file=stop_words_file)
+	flat_token_list = utils.text.get_flat_tokens(token_lists)
+	flat_sentence_num = utils.text.get_flat_sentence_num(token_lists)
+	flat_pos_tags = utils.text.get_flat_pos_tags(token_lists)
+	flat_token_lens = utils.text.get_token_lens(flat_token_list)  # word length
+
+	pronoun_ratios = utils.text.get_pronoun_ratio(flat_sentence_num, flat_pos_tags)
+	word_num_list = utils.text.get_flat_word_num(token_lists)  # word no. within sentence
+
+	nonletters = utils.text.get_nonletters(flat_token_list, exceptions=[]) # find all non-letter characters in file
+	flat_cleaned_token_list = utils.text.strip_words(flat_token_list, method='punctuation', nonletters=nonletters) # clean words: strip nonletters/punctuation and lowercase
+	flat_lemmatized_token_list = utils.text.get_lemmatized_tokens(flat_cleaned_token_list, flat_pos_tags, utils.text.pos_for_lemmatization)  # lemmas
+	flat_is_content_word = utils.text.get_is_content(flat_pos_tags, content_pos=utils.text.pos_for_content) # content or function word
+
+
 	surprisal_database = 'pickle/surprisal-3_dict.pkl' # default: 3-gram surprisal
 	features_ignore_case = True
 	features_transform = ['default', None, None] # format: [method, cols to log transform, cols to z-score] (if method is specified, the other two are ignored)
 
-	# Word features
-	wordlst = utils.get_wordlst(sent_tokens) # raw words
-	taglst = utils.get_pos_tag(sent_tokens) # POS tag
-	numWords = len(wordlst)
-	numSentences = len(sent_tokens)
-	setlst = [3]*numWords # set no. 
-	sent_num_list = utils.get_sent_num(sent_tokens) # sentence no.
-
-	# Get pronoun ratio
-	pronoun_ratios = utils.get_pronoun_ratio(sent_num_list, taglst)
-
 	# Get morpheme from polyglot library instead of library
-	poly_morphemes = utils.get_poly_morpheme(sent_num_list, wordlst)
+	# TODO: where was this supposed to be used?
+	# poly_morphemes = utils.get_poly_morpheme(flat_sentence_num, flat_token_list)
 
-	word_num_list = utils.get_word_num(sent_tokens) # word no. within sentence
-
-	nonletters = utils.get_nonletters(wordlst, exceptions=[]) # find all non-letter characters in file
-	wordlst_l = utils.strip_words(wordlst, method=clean_word_method, nonletters=nonletters) # clean words: strip nonletters/punctuation and lowercase
-	print("Number of sentences:", numSentences)
-	print("Number of words:", numWords)
-	print("Number of unique words (cleaned):", len(set(wordlst_l)))
-	print(f"Average number of words per sentence: {numWords/numSentences:.2f}")
-	print(utils.get_divider())
-
-	wordlen = utils.get_wordlen(wordlst_l) # word length
-	wordlst_lem = utils.get_lemma(wordlst_l, taglst, lemmatized_pos) # lemmas
 	
-	# Get is content boolean
-	is_content_lst = utils.get_is_content(taglst, content_pos=content_pos) # content or function word
+	# Word features
+	n_words = len(flat_token_list)
+	n_sentences = len(token_lists)
+	# TODO what is this?
+	setlst = [3] * n_words # set no. 
+	print("Number of sentences:", n_sentences)
+	print("Number of words:", n_words)
+	print("Number of unique words (cleaned):", len(set(flat_cleaned_token_list)))
+	print(f"Average number of words per sentence: {n_words/n_sentences:.2f}")
+	print('-'*79)
+
 	
 	# Updated features
 	#databases = utils.load_databases(ignore_case=features_ignore_case)
-	if calc_lex == True:
-		print('Estimating lexical and syntactical features')
-		
-		merged_vals = utils.get_all_features_merged(wordlst_l, wordlst_lem, databases) # lexical features
-		# Clear variables so we have RAM
-		del databases
-		
-		# Results
-		result = utils.compile_results(wordlst, wordlst_l, wordlst_lem, 
-										taglst, is_content_lst, setlst, 
-										sent_num_list, wordlen, merged_vals)
+	if lexical == True:
+		sentspace.lexical.get_lexical_features(None)
+		exit()
 	
-		result = utils.transform_features(result, *features_transform)
-	
-		features_used = ['Age of acquisition', 'Arousal', 'Concreteness', 
-						'Contextual diversity (log)', 'Degree centrality (log)', 
-						'Frequency of orthographic neighbors (log)', 'Lexical decision RT', 
-						'Lexical frequency (log)', 'Lexical surprisal', 'Number of morphemes', 'Number of morphemes poly',
-						'Orthography-Semantics Consistency', 'Prevalence', 'Valence', 'Word length','Polysemy']
-	
-		print('Computing sentence embeddings')
-		sent_embed = utils.get_sent_vectors(result, features_used, embed_method, 
-		                                   content_only=content_only, 
-		                                   pronoun_ratios=pronoun_ratios,
-		                                   )
-		lex_per_word_with_uniform_column = utils.conform_word_lex_df_columns(result)
-		lex_per_word_with_uniform_column.to_csv(word_lex_output_path,index=False)
-		
-		print('Writing lex sentence embedding to csv at '+ embed_lex_output_path)
-		sent_embed.to_csv(embed_lex_output_path, index=False)
-	
-		# Make the syntax excel sheet
-		print('Writing syntax sentence embedding to csv at '+ sent_output_path)
-		#pdb.set_trace()
-		
-		# Read in benchmark data
-		df_benchmark = pd.read_csv(path_2_bechmark)
-	
-		# Return percentile per sentence for each 
-		percentile_df = utils.return_percentile_df(df_benchmark, sent_embed)
-		print('Writing percentiles')
-		percentile_df.to_csv(bench_perc_out_path,index=False)
-	
-	if calc_syntax:
+	if syntax:
+		exit()
 		# Get content ratio
-		content_ratios = utils.get_content_ratio(sent_num_list, is_content_lst)
+		content_ratios = sentspace.syntax.get_content_ratio(flat_sentence_num, flat_is_content_word)
 
-		syn_feats = sentspace.syntax.get_features('hello my name is syntax', dlt=True, left_corner=True)
+		syn_feats = sentspace.syntax.get_tree_features('hello my name is syntax', dlt=True, left_corner=True)
 		print(syn_feats)
 
 		syntax_df = pd.DataFrame(
 			{'Sentence no.': content_ratios['sent_num'], 'Content Ratio': content_ratios['content_ratio']})
 		syntax_df.to_csv(sent_output_path, index=False)
 
-
-	# DEBUG
-	exit()
 
 	# Clear vars for RAM
 	if 'databases' in locals():
@@ -236,7 +185,7 @@ def estimate_sentence_embeddings(path_2_sentence_file, path_2_stop_words, path_2
 	# utils.pPMI(sent_rows, pmi_paths)
 	# pdb.set_trace()
 	
-	if calc_glove == True:
+	if embedding == True:
 		# Get GloVE
 		result = utils.compile_results_for_glove_only(wordlst, wordlst_l, wordlst_lem, 
 										taglst, is_content_lst, setlst, 
@@ -279,8 +228,11 @@ def main(args):
 	'''
 
 	# Estimate sentence embeddings
-	estimate_sentence_embeddings(args.input_file, path_2_stop_words=args.stop_words, path_2_bechmark=args.benchmark,
-                              calc_lex=args.calc_lex, calc_syntax=args.calc_syntax, calc_glove=args.calc_glove)
+	features = run_sentence_features_pipeline(args.input_file, stop_words_file=args.stop_words, 
+											  benchmark_file=args.benchmark, lexical=args.lexical, 
+											  syntax=args.syntax, embedding=args.embedding,
+											  semantic=args.semantic,
+											  out_dir=args.out_dir)
 	#estimate_sentence_embeddings(args.input_file)
 
 
@@ -288,8 +240,7 @@ if __name__ == "__main__":
 
 	# Parse input
 	parser = argparse.ArgumentParser('sentspace', 
-                             	     usage=
-		"""
+                             	     usage="""
 		Example run:
 			python3 -m sentspace -i example/example.csv
 
@@ -304,26 +255,37 @@ if __name__ == "__main__":
 		"""
     )
 
-	parser.add_argument('-i', '--input_file', nargs='?', type=argparse.FileType('r'), 
-						default=sys.stdin,
-						help='Path to input file or a single sentence. If '
+	parser.add_argument('-i', '--input_file', type=str,
+						default=None,
+						required=True,
+						help='path to input file or a single sentence. If '
 						     'supplying a file, it must be .csv .txt or .xlsx,'
 							 ' e.g., example/example.csv')
 
 	# Add an option for a user to include their own stop words
-	parser.add_argument('-sw','--stop_words', default=None, 
-						help='Path to delimited file of words to filter out from analysis, e.g., example/stopwords.txt')
+	parser.add_argument('-sw','--stop_words', default=None, type=str, 
+						help='path to delimited file of words to filter out from analysis, e.g., example/stopwords.txt')
 	
 	# Add an option for a user to choose their benchmark
-	parser.add_argument('-b', '--benchmark', default='sentspace/benchmarks/lex/UD_corpora_lex_features_sents_all.csv', 
-						help='Path to csv file of benchmark corpora For example benchmarks/lex/UD_corpora_lex_features_sents_all.csv')
+	parser.add_argument('-b', '--benchmark', type=str,
+						default='sentspace/benchmarks/lexical/UD_corpora_lex_features_sents_all.csv',
+                     	help='path to csv file of benchmark corpora For example benchmarks/lexical/UD_corpora_lex_features_sents_all.csv')
+
+	# parser.add_argument('--cache_dir', default='.cache', type=str,
+    #                  	help='path to directory where results may be cached')
+
+	parser.add_argument('-o', '--out_dir', default='./sentspace_output', type=str,
+                     	help='path to output directory where results may be stored')
 
 	# Add an option for a user to choose to not do some analyses. Default is true
-	parser.add_argument('-l','--lexical', type = strtobool, default=True, help='calculate lexical features? [True]')
-	parser.add_argument('-s','--syntax', type = strtobool, default=True, help='calculate syntactic features? [True]')
-	parser.add_argument('-g','--glove', type = strtobool, default=True, help='compute glove embeddings? [True]')
+	parser.add_argument('-lx','--lexical', type=strtobool, default=True, help='compute lexical features? [True]')
+	parser.add_argument('-sx','--syntax', type=strtobool, default=True, help='compute syntactic features? [True]')
+	parser.add_argument('-em','--embedding', type=strtobool, default=True, help='compute sentence embeddings? [True]')
+	parser.add_argument('-sm','--semantic', type=strtobool, default=True, help='compute semantic (multi-word) features? [True]')
 	
 	args = parser.parse_args()	
-	print(args)
+	print('-'*79)
+	print('Received arguments:', args)
+	print('-'*79)
 
 	main(args)
