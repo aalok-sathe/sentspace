9b748da5df5d2d3f02d7dca522c739940d4dfb1b
aalok-sathe <asathe@mit.edu>
2021-08-03 17:49:56 -0400

    make utils into a submodulke

---
+++ sentspace/utils/__init__.py
@@ -0,0 +1,6 @@
+
+import sentspace.utils.io as io
+import sentspace.utils.caching as caching
+import sentspace.utils.text as text
+import sentspace.utils.sanity_checks as sanity_checks
+from sentspace.utils.utils import *
