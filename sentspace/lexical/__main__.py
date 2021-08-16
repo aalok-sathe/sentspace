498f0a8ce9b6802b81752b4dbca1c7bd33bc368d
asathe <asathe@mit.edu>
2021-08-16 10:56:56 -0400

    reorganize how input is obtained and processed in submodule main scripts

--- sentspace/lexical/__main__.py
+++ sentspace/lexical/__main__.py
@@ -1,37 +1,50 @@
 import argparse
 import json
 from sentspace.lexical import get_features
+import sentspace.utils.io
 from distutils.util import strtobool
 import pandas as pd
+from pathlib import Path
 
 def main(args):
     
-    features = get_features(args.input.split())
-    print(pd.DataFrame(features))
-    # print(json.dumps(features, indent=4))
+    split = lambda s: s.split()
+
+    if Path(args.input).exists():
+        with Path(args.input).open('r') as f:
+            input = [*map(split, f.readlines())]
+    else:
+        input = [*map(split, args.input.split('\n'))]
+
+    for i, item in enumerate(input):
+        sentspace.utils.io.log(f'*** processing sentence {i}/{len(input)} ***')
+        sentspace.utils.io.log(f'--- {item}')
+        features = get_features(item)
+        sentspace.utils.io.log(f'--- obtained features:')
+        if args.output == 'pandas':
+            print(pd.DataFrame(features))
+        elif args.output == 'json':
+            print(json.dumps(features, indent=4))
+        else:
+            raise ValueError(f'output mode {args.output} not in supported modes (pandas, json)')
 
 
 if __name__ == '__main__':
     # Parse input
     parser = argparse.ArgumentParser('sentspace.lexical')
 
-    parser.add_argument('input', type=str, #argparse.FileType('r'), 
-                        # default=sys.stdin,
-                        help='File (currently NotImplemented) or single sentence to process')
+    parser.add_argument('input', type=str, help='File or newline-separated sentences to process')
+    parser.add_argument('-o', '--output', type=str, default='pandas',
+                        help='Output mode pandas/json/yaml(not implemented)')
 
     # Add an option for a user to include their own stop words
-    parser.add_argument('-sw', '--stop_words', default=None,
-                        help='Path to delimited file of words to filter out from analysis. For example example/stopwords.txt')
+    # parser.add_argument('-sw', '--stop_words', default=None,
+    #                     help='Path to delimited file of words to filter out from analysis. For example example/stopwords.txt')
 
     # Add an option for a user to choose their benchmark
-    parser.add_argument('-b', '--benchmark', default='sentspace/benchmarks/lex/UD_corpora_lex_features_sents_all.csv',
-                        help='Path to csv file of benchmark corpora For example benchmarks/lex/UD_corpora_lex_features_sents_all.csv')
+    # parser.add_argument('-b', '--benchmark', default='sentspace/benchmarks/lex/UD_corpora_lex_features_sents_all.csv',
+    #                     help='Path to csv file of benchmark corpora For example benchmarks/lex/UD_corpora_lex_features_sents_all.csv')
 
-    # Add an option for a user to choose to not do some analyses. Default is true
-    parser.add_argument('-dlt', '--dlt', type=strtobool,
-                        default=True, help='calculate dependency locality theory (DLT) metric? [True]')
-    parser.add_argument('-lc', '--left_corner', type=strtobool,
-                        default=True, help='calculate left corner metric? [True]')
 
     args = parser.parse_args()
     print(args)
--- sentspace/syntax/__main__.py
+++ sentspace/syntax/__main__.py
@@ -1,12 +1,33 @@
 import argparse
-
+import json
 from distutils.util import strtobool
+
+import pandas as pd
 from sentspace.syntax import get_features
+import sentspace.utils.io
+
 
 def main(args):
-    
-    features = get_features(args.input, dlt=args.dlt, left_corner=args.left_corner)
-    print(features)
+
+    split = lambda s: s.split()
+
+    if Path(args.input).exists():
+        with Path(args.input).open('r') as f:
+            input = [*map(split, f.readlines())]
+    else:
+        input = [*map(split, args.input.split('\n'))]
+
+    for i, item in enumerate(input):
+        sentspace.utils.io.log(f'*** processing sentence {i}/{len(input)} ***')
+        sentspace.utils.io.log(f'--- {item}')
+        features = get_features(item, dlt=args.dlt, left_corner=args.left_corner)        
+        sentspace.utils.io.log(f'--- obtained features:')
+        if args.output == 'pandas':
+            print(pd.DataFrame(features))
+        elif args.output == 'json':
+            print(json.dumps(features, indent=4))
+        else:
+            raise ValueError(f'output mode {args.output} not in supported modes (pandas, json)')
 
 
 if __name__ == '__main__':
@@ -14,17 +35,17 @@ if __name__ == '__main__':
     # Parse input
     parser = argparse.ArgumentParser('sentspace.syntax')
 
-    parser.add_argument('input', type=str, #argparse.FileType('r'), 
-                        # default=sys.stdin,
-                        help='a single sentence to process')
+    parser.add_argument('input', type=str, help='File or newline-separated sentences to process')
+    parser.add_argument('-o', '--output', type=str, default='pandas',
+                        help='Output mode pandas/json/yaml(not implemented)')
 
     # Add an option for a user to include their own stop words
-    parser.add_argument('-sw', '--stop_words', default=None,
-                        help='Path to delimited file of words to filter out from analysis. For example example/stopwords.txt')
+    # parser.add_argument('-sw', '--stop_words', default=None,
+    #                     help='Path to delimited file of words to filter out from analysis. For example example/stopwords.txt')
 
     # Add an option for a user to choose their benchmark
-    parser.add_argument('-b', '--benchmark', default='sentspace/benchmarks/lex/UD_corpora_lex_features_sents_all.csv',
-                        help='Path to csv file of benchmark corpora For example benchmarks/lex/UD_corpora_lex_features_sents_all.csv')
+    # parser.add_argument('-b', '--benchmark', default='sentspace/benchmarks/lex/UD_corpora_lex_features_sents_all.csv',
+    #                     help='Path to csv file of benchmark corpora For example benchmarks/lex/UD_corpora_lex_features_sents_all.csv')
 
     # Add an option for a user to choose to not do some analyses. Default is true
     parser.add_argument('-dlt', '--dlt', type=strtobool,
