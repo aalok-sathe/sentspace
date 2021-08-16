dd484b08f8eb726d0d74e6357fd31525206d1158
asathe <asathe@mit.edu>
2021-08-16 13:03:36 -0400

    revert to original parser command

--- sentspace/syntax/utils/parse_trees.sh
+++ sentspace/syntax/utils/parse_trees.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-if [[ -f $1 ]]; then
-    input=$(cat $1)
+if [[ -f $2 ]]; then
+    input=$(cat $2)
 else
-    input=$1
+    input=$2
 fi
 
-echo $input | ./parser-fullberk  wsj02to21.gcg15.prtrm.4sm.fullberk.model | perl -pe 's/\+(?=[^\)]* )/\-/g'  |  perl -pe 's/^ *\( *//;s/ *\) *$//;s/-\d+ / /g;s/-PRTRM//g'
+# echo $(date) "beam_width param received -c $1 $2" >> parse_trees.log
 
+# pristine original
+echo $input | ./parser-fullberk  wsj02to21.gcg15.prtrm.4sm.fullberk.model| perl -pe 's/\+(?=[^\)]* )/\-/g'  |  perl -pe 's/^ *\( *//;s/ *\) *$//;s/-\d+ / /g;s/-PRTRM//g'
+# modified version
+# echo $input | java -Xmx8g -cp berkeleyparser/berkeleyParser.jar edu.berkeley.nlp.PCFGLA.BerkeleyParser -substates -gr wsj02to21.gcg15.prtrm.4sm.fullberk.model -c $1 | perl -pe 's/\+(?=[^\)]* )/\-/g'  |  perl -pe 's/^ *\( *//;s/ *\) *$//;s/-\d+ / /g;s/-PRTRM//g'
