393b2f4e459d6f757d07cce8c0c628304f605fd0
asathe <asathe@mit.edu>
2021-08-16 11:34:59 -0400

    change example file function calls by new convention

--- sentspace/syntax/compute_tree_dlt_left_corner.py
+++ sentspace/syntax/compute_tree_dlt_left_corner.py
@@ -1,4 +1,4 @@
-import utils
+import sentspace.syntax
 
 text = ("When Sebastian Thrun started working on self-driving cars at "
         "Google in 2007, few people outside of the company took him "
@@ -7,10 +7,10 @@ text = ("When Sebastian Thrun started working on self-driving cars at "
         "worth talking to,” said Thrun, in an interview with Recode earlier "
         "this week.")
 
-features = utils.pipeline(text, dlt=True, left_corner=True)
+features = sentspace.syntax.get_features(text, dlt=True, left_corner=True)
 print(features.dlt)
 print(features.tree)
 print(features.left_corner)
 
-output = utils.pipeline(text, dlt=True)  # === output = utils.pipeline(text, dlt=True, left_corner = False)
-print(output.left_corner)  # is None
+# output = utils.pipeline(text, dlt=True)  # === output = utils.pipeline(text, dlt=True, left_corner = False)
+# print(output.left_corner)  # is None
