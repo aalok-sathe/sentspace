0247354eac1ecf47dc1a51e4069b6033396c65e0
asathe <aalok.sathe@richmond.edu>
2021-08-19 12:05:03 -0400

    move nltk downloads to top of utils file to avoid circular rep

--- sentspace/utils/__init__.py
+++ sentspace/utils/__init__.py
@@ -1,9 +1,9 @@
 
+from sentspace.utils.utils import *
+download_nltk_resources()
+
 import sentspace.utils.io as io
 import sentspace.utils.caching as caching
 import sentspace.utils.text as text
 # import sentspace.utils.sanity_checks as sanity_checks
-from sentspace.utils.utils import *
 
-
-download_nltk_resources()
