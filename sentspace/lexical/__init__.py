2d82554279f79f9b331eafa42ce51c8021adec61
aalok-sathe <asathe@mit.edu>
2022-07-05 10:06:36 -0400

    syntax pipeline now accepts sentspace.Sentence in line with API

--- sentspace/lexical/__init__.py
+++ sentspace/lexical/__init__.py
@@ -13,24 +13,27 @@ def get_features(sentence: sentspace.Sentence.Sentence, lock=None) -> dict:
     # io.log(f'computing lexical featuures for `{sentence}`')
 
     # if lock: lock.acquire()
-    databases = utils.load_databases(features='all')
+    databases = utils.load_databases(features="all")
     # if lock: lock.release()
 
-    features_from_database = utils.get_all_features(sentence, databases)  # lexical features
-    
+    features_from_database = utils.get_all_features(
+        sentence, databases
+    )  # lexical features
+
     # TODO[?] return dict of lists, to be consistent with API?
     # return list of token-level features, as a dict per token
-    returnable = []
     for i, token in enumerate(sentence.tokens):
-        db_features_slice = {feature: features_from_database[feature][i] for feature in features_from_database}
-        returnable += [{'index': sentence.uid,
-                        'sentence': str(sentence),
-                        'token': token,
-                        'lemma': sentence.lemmas[i],
-                        'tag': sentence.pos_tags[i],
-                        'content_word': sentence.content_words[i],
-                        **db_features_slice
-                       }]
+        db_features_slice = {
+            feature: features_from_database[feature][i]
+            for feature in features_from_database
+        }
 
-    return returnable
-
+        yield {
+            "index": sentence.uid,
+            "sentence": str(sentence),
+            "token": token,
+            "lemma": sentence.lemmas[i],
+            "tag": sentence.pos_tags[i],
+            "content_word": sentence.content_words[i],
+            **db_features_slice,
+        }
